7b32c02c85aa0de5dddee477560f771950bac84d
jagues6 <alexguevara31@gmail.com>
2026-09-04 16:15:28 -0500

    modificacion en database.js

--- database.js
+++ database.js
@@ -5,17 +5,24 @@
  * @function dbConnection
  * @returns {Promise<void>}
  */
+import dns from "dns";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
+// Las cadenas "mongodb+srv://" hacen una consulta DNS especial (tipo SRV). En
+// algunas redes el DNS local la rechaza (error "querySrv ECONNREFUSED") y la
+// conexion nunca se logra, aunque en MongoDB Compass funcione. Forzar un DNS
+// publico (Google y Cloudflare) resuelve ese caso y no afecta a otras redes.
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Conectado a la base de datos");
-  } catch {
-    console.log("Error conectando a la base de datos, reintentando en 5s...");
+  } catch(e) {
+    console.log("Error conectando a la base de datos, reintentando en 5s... "+e);
     setTimeout(() => {
       dbConnection();
     }, 5000);
